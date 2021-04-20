a0c038169721a285b130a82bb2d47d68c5a01b22
Gašper Rozman <gasper.rozman@it-tim.si>
2021-04-20 14:57:46 +0200

    Initial Commit

---
+++ vue2shelt/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+      <PlanetEarth/>
+  </div>
+</template>
+
+<script>
+import PlanetEarth from './components/PlanetEarth.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PlanetEarth
+  }
+}
+</script>
